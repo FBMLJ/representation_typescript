b462c0077a8de07e967a491d4c1ba0daeff85c9a
Lucas Tavares <lucastavasousa@gmail.com>
2024-10-30 21:36:05 -0300

    teste

--- src/representation/investimentoRepresentation.ts
+++ src/representation/investimentoRepresentation.ts
@@ -1,5 +1,22 @@
 import { notNull } from "projeto_validacao";
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Investimento:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: O ID do investimento
+ *         valor:
+ *           type: number
+ *           description: O valor do investimento
+ *         descricao:
+ *           type: string
+ *           description: A descrição do investimento
+ */
 export default class InvestimentoRepresentation {
     @notNull()
     name: string = ""
