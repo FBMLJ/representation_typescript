2209da801b1f7a04b8be4196f5b6108e4ca0664c
Lucas Tavares <lucas.sousa@serpro.gov.br>
2024-11-01 15:47:12 -0300

    resolvendo problema de tipo

--- src/representation/responseBody/MessagemPadraoResponse.ts
+++ src/representation/responseBody/MessagemPadraoResponse.ts
@@ -1,4 +1,7 @@
+import { field } from "projeto_validacao";
+
 export class MessagemPadraoReponse {
+    @field({type: "string",example: "processo realizado com sucesso"})
     mensagem!:string
     constructor(mensagem: string){
         this.mensagem = mensagem;
